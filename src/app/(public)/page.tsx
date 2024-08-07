77ac37fbceff79df50437d8acebd8c982c88f474
Nader <nader.elmahdy@xapprika.com>
2024-08-07 12:40:41 +0300

    chore: Refactor layout and navigation logic in coffee shop module

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,36 @@
+import { getUser } from "@/utils/get-user";
 import { redirect } from "next/navigation";
-
-export default function Home() {
-  redirect("/coffee-shop/dashboard");
+import prisma from "@/lib/prisma";
+export default async function Home() {
+  const sessionUser = await getUser();
+  const user = sessionUser
+    ? await prisma.user.findUnique({
+        where: { id: sessionUser?.id },
+        select: { role: true },
+      })
+    : null;
+  if (user?.role === "PLATFORM_ADMIN") {
+    redirect("/dashboard");
+  }
+  if (user?.role === "SHOP_ADMIN") {
+    const userShops = await prisma.coffeeShop.findMany({
+      where: { adminId: sessionUser?.id },
+    });
+    if (!userShops || userShops.length === 0) {
+      const shop = await prisma.coffeeShop.create({
+        data: {
+          name: `${sessionUser?.name}'s Shop`,
+          adminId: sessionUser?.id as string,
+          address: "My Address",
+          revenueShare: 0.1,
+          workingHours: {
+            sunday: { open: 0, close: 0 },
+          },
+        },
+      });
+    }
+    redirect("/coffee-shop/dashboard");
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <h1 className="text-4xl font-bold">Next.js</h1> */}
