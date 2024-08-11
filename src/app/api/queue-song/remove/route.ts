44ed45e9afed94430aba7d76d38dc0c46ec3c80e
Nader <nader.elmahdy@xapprika.com>
2024-08-11 14:06:19 +0300

    fixed remove from queue endpoint

--- src/app/api/queue-song/remove/route.ts
+++ src/app/api/queue-song/remove/route.ts
@@ -2,23 +2,58 @@ import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const { songId, coffeeShopId } = await req.json();
-  const queueSong = await prisma.queueSong.findFirst({
-    where: {
-      songId,
-      coffeeShopId,
-    },
-  });
-  if (!queueSong) {
+  const { queueSongId, coffeeShopId } = await req.json();
+
+  if (!queueSongId) {
     return NextResponse.json(
-      { error: "Queue song not found" },
-      { status: 404 }
+      { error: "queueSongId is required" },
+      { status: 400 }
     );
   }
-  await prisma.queueSong.delete({
-    where: {
-      id: queueSong.id,
-    },
-  });
-  return NextResponse.json({ deleted: true, queueSong });
+
+  if (!coffeeShopId) {
+    return NextResponse.json(
+      { error: "coffeeShopId is required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const queueSong = await prisma.queueSong.delete({
+      where: {
+        id: queueSongId,
+      },
+      include: {
+        song: true,
+      },
+    });
+
+    if (!queueSong) {
+      throw new Error("Queue song not found");
+    }
+    const queue = await prisma.queueSong.findMany({
+      where: {
+        coffeeShopId,
+      },
+      include: {
+        song: true,
+      },
+    });
+
+    return NextResponse.json({ deleted: true, deletedSong: queueSong, queue });
+  } catch (error: any) {
+    if (error.code === "P2025") {
+      // Prisma error code for record not found
+      return NextResponse.json(
+        { error: "Queue song not found" },
+        { status: 404 }
+      );
+    } else {
+      // Handle other errors (e.g., Prisma connection errors)
+      return NextResponse.json(
+        { error: "An unexpected error occurred" },
+        { status: 500 }
+      );
+    }
+  }
 }
