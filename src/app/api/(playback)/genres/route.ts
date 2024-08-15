8db0eef70b463eeca6ce907a314b489bcba62387
Abdelrhman Samy Saad <abdelrhman.samy.saad@gmail.com>
2024-08-15 14:26:49 +0300

    chore: Add error handling to GET request in genres route

--- src/app/api/(playback)/genres/route.ts
+++ src/app/api/(playback)/genres/route.ts
@@ -1,8 +1,12 @@
-import prisma from "@/lib/prisma";
-import { revalidatePath } from "next/cache";
-import { NextResponse, NextRequest } from "next/server";
+import prisma from '@/lib/prisma'
+import { revalidatePath } from 'next/cache'
+import { NextResponse, NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
-  const genres = await prisma.genre.findMany();
-  return NextResponse.json(genres);
+  try {
+    const genres = await prisma.genre.findMany()
+    return NextResponse.json(genres)
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 400 })
+  }
 }
