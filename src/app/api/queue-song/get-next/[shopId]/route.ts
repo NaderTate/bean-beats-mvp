e52137d796dd3f81d1b60e79bb02e81971acf466
nadertate <nadertate@gmail.com>
2024-09-12 11:06:20 +0300

    Refactor queue-song route to handle errors and improve code readability

--- src/app/api/queue-song/get-next/[shopId]/route.ts
+++ src/app/api/queue-song/get-next/[shopId]/route.ts
@@ -5,42 +5,50 @@ export async function GET(
   request: NextRequest,
   { params: { shopId } }: { params: { shopId: string } }
 ) {
-  // get the first song in the queue
-  const song = await prisma.queueSong.findFirst({
-    where: {
-      coffeeShopId: shopId,
-    },
-    orderBy: {
-      id: "asc",
-    },
-  });
+  try {
+    // get the first song in the queue
+    const song = await prisma.queueSong.findFirst({
+      where: {
+        coffeeShopId: shopId,
+      },
+      orderBy: {
+        id: "asc",
+      },
+    });
 
-  if (!song) {
+    if (!song) {
+      return NextResponse.json(
+        { message: "No songs in the queue" },
+        { status: 404 }
+      );
+    }
+
+    // remove the song from the queue
+    await prisma.queueSong.delete({
+      where: {
+        id: song.id,
+      },
+    });
+
+    // get the updated queue
+    const queue = await prisma.queueSong.findMany({
+      where: {
+        coffeeShopId: shopId,
+      },
+      include: {
+        song: true,
+      },
+    });
+
+    // return the next 2 songs in the queue
+    const nextSongs = queue.slice(0, 2);
+
+    return NextResponse.json(nextSongs);
+  } catch (error) {
+    console.error("Error fetching or processing the queue:", error);
     return NextResponse.json(
-      { message: "No songs in the queue" },
-      { status: 404 }
+      { message: "An error occurred while processing the queue" },
+      { status: 500 }
     );
   }
-
-  // remove the song from the queue
-  await prisma.queueSong.delete({
-    where: {
-      id: song.id,
-    },
-  });
-
-  // get the updated queue
-  const queue = await prisma.queueSong.findMany({
-    where: {
-      coffeeShopId: shopId,
-    },
-    include: {
-      song: true,
-    },
-  });
-
-  // return the next 2 songs in the queue
-  const nextSongs = queue.slice(0, 2);
-
-  return NextResponse.json(nextSongs);
 }
