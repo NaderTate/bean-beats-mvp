24007416a8dc091b9810add91913c4a58837450d
nadertate <nadertate@gmail.com>
2024-09-17 16:10:28 +0300

    Refactor GenrePage component to use GenreMain component for displaying genre information and songs

---
+++ src/app/[lang]/(public)/shop/[shopId]/music/genres/[genreId]/main.tsx
@@ -0,0 +1,61 @@
+import { Genre, Song, SongCoffeeShop } from "@prisma/client";
+import { useTranslations } from "next-intl";
+import React from "react";
+import SongCard from "../../song-card";
+import Image from "next/image";
+import { FaMusic } from "react-icons/fa";
+
+type Props = {
+  genre: ({ songs: Song[] } & Genre) | null;
+  SongCoffeeShop: SongCoffeeShop[];
+};
+
+const GenreMain = ({ genre, SongCoffeeShop }: Props) => {
+  const t = useTranslations();
+
+  if (!genre) {
+    return (
+      <div className="h-screen flex flex-col items-center justify-center">
+        <Image
+          src="/images/not-found.svg"
+          width={200}
+          height={200}
+          alt="not-found"
+        />
+        <h1>{t("Genre not found")}</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="mt-24 px-14">
+      <Image
+        src={genre.image}
+        className="w-full h-72 object-cover rounded-lg"
+        width={2000}
+        height={1000}
+        alt={genre.name}
+      />
+      <div className="flex justify-between">
+        <h1 className="text-xl text-primary font-bold mt-4">{genre.name}</h1>
+        <p className="text-lg mt-4">
+          <FaMusic className="inline-block mr-2 text-primary" />
+          {genre.songs.length} {genre.songs.length === 1 ? "track" : "tracks"}
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+        {genre.songs.map((song) => (
+          <SongCard
+            key={song.id}
+            song={{
+              ...song,
+              price:
+                SongCoffeeShop.find((s) => s.songId === song.id)?.price || 0.25,
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GenreMain;
--- src/app/[lang]/(public)/shop/[shopId]/music/genres/[genreId]/page.tsx
+++ src/app/[lang]/(public)/shop/[shopId]/music/genres/[genreId]/page.tsx
@@ -1,7 +1,5 @@
 import prisma from "@/lib/prisma";
-import SongCard from "../../song-card";
-import Image from "next/image";
-import { FaMusic } from "react-icons/fa";
+import GenreMain from "./main";
 
 type GenrePageProps = { params: { shopId: string; genreId: string } };
 
@@ -17,43 +15,8 @@ const GenrePage = async ({ params: { genreId, shopId } }: GenrePageProps) => {
       coffeeShopId: shopId,
     },
   });
-  if (!genre) {
-    return (
-      <div>
-        <h1>Genre not found</h1>
-      </div>
-    );
-  }
-  return (
-    <div className="mt-24 px-14">
-      <Image
-        src={genre.image}
-        className="w-full h-72 object-cover rounded-lg"
-        width={2000}
-        height={1000}
-        alt={genre.name}
-      />
-      <div className="flex justify-between">
-        <h1 className="text-xl text-primary font-bold mt-4">{genre.name}</h1>
-        <p className="text-lg mt-4">
-          <FaMusic className="inline-block mr-2 text-primary" />
-          {genre.songs.length} {genre.songs.length === 1 ? "track" : "tracks"}
-        </p>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
-        {genre.songs.map((song) => (
-          <SongCard
-            key={song.id}
-            song={{
-              ...song,
-              price:
-                SongCoffeeShop.find((s) => s.songId === song.id)?.price || 0.25,
-            }}
-          />
-        ))}
-      </div>
-    </div>
-  );
+
+  return <GenreMain genre={genre} SongCoffeeShop={SongCoffeeShop} />;
 };
 
 export default GenrePage;
